eb97cdef1e86260856161f792393f0f648c6404f
Pushkar yadav <58500609+Pufo-49@users.noreply.github.com>
2020-11-19 17:30:22 +0530

    Add files via upload

---
+++ Author_model.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+
+// Create Schema
+const BooksSchema =new Schema({
+  title:String,
+  pages:Number
+
+});
+
+const AuthorSchema=new Schema({
+ name:String,
+ age:Number,
+ books:[BooksSchema]
+
+
+});
+
+
+const Author=mongoose.model('author',AuthorSchema);
+
+module.exports=Author;
